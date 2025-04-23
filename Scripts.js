46acbcf533d26453fa5c2399fdabcca61c08ba4d
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MBP.localdomain>
2025-04-23 16:16:05 +0100

    Added functionality

--- Scripts.js
+++ Scripts.js
@@ -3,7 +3,12 @@ function OpenMenu(){
     console.log("active");
     var NavCapsule = document.getElementById("FancyNav");
     var NavBtn = document.getElementById("NavBtn");
+    var Links =document.querySelectorAll("Links");
 
     NavBtn.classList.toggle("btnActive");
     NavCapsule.classList.toggle("menu");
+
+    Links.forEach(function(el, key){
+        Links.classList.toggle("show");        
+    });
 }
