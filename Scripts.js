c9a4ddf27e0609b569150885199b991f4708ce16
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MacBook-Pro.local>
2025-05-01 10:54:55 +0100

    Added fade in and rotation

--- Scripts.js
+++ Scripts.js
@@ -1,14 +1,13 @@
 
 function OpenMenu(){
     console.log("active");
+
     var NavCapsule = document.getElementById("FancyNav");
     var NavBtn = document.getElementById("NavBtn");
-    var Links =document.querySelectorAll("Links");
-
+    
     NavBtn.classList.toggle("btnActive");
     NavCapsule.classList.toggle("menu");
-
-    Links.forEach(function(el, key){
-        Links.classList.toggle("show");        
-    });
+    
+    var Links =document.querySelectorAll(".Links");
+    Links.forEach(elem => elem.classList.toggle("show"));
 }
